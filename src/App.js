92230f93a17c43de092fa4e4cd08d1f0d85268c2
cMELVINrb <barsanacm@gmail.com>
2019-01-14 14:13:06 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Todos from './components/Todos'
+import './App.css'; //serves as the global css
 
 class App extends Component {
-  render() {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Start a meeting',
+                execution: 'Inform all group members via email'
+            },
+            {
+                id: 2,
+                title: 'Start a meeting',
+                execution: 'Inform all group members via email'
+            },
+            {
+                id: 3,
+                title: 'Start a meeting',
+                execution: 'Inform all group members via email'
+            }
+        ]
+    }
+
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos />
       </div>
     );
   }
